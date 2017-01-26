a64f52d3e4d0208427e396e99aac8a85b8a12cf6
Yotam <yotam.rt@gmail.com>
2017-01-26 17:57:34 +0200

    Add Data To Body

--- client/components/PivotBody/index.js
+++ client/components/PivotBody/index.js
@@ -13,7 +13,6 @@ export default class PivotBody extends Component {
 
   addToPivot(){
     const {headersData, hierarchy} = this.props
-    console.log(headersData);
     if (headersData && headersData.rows){
       let headersPart = headersData.rows
 
@@ -52,8 +51,58 @@ export default class PivotBody extends Component {
       }).reduce((res,cur)=>res.concat(cur), [])
     }
 
-    console.log(headersDataParts[6], transformer.getColPosition(hierarchy, headersData.cols, headersDataParts[6]));
-    console.log(headersDataParts[6], transformer.getRowPosition(hierarchy, headersData.rows, headersDataParts[6]));
+    const matrixColsCount = Object.keys(headersData.cols).reduce((res, curr)=> {
+      return res + headersData.cols[curr][LEAF_CHILDREN_COUNT_SYM]
+    },0)
+
+    const matrixRowsCount = Object.keys(headersData.rows).reduce((res, curr)=> {
+      return res + headersData.rows[curr][LEAF_CHILDREN_COUNT_SYM]
+    },0)
+
+    const measuresCount = hierarchy.filter((curr)=> {
+      return curr.type === 'measures'
+    }).length
+
+    let matrix = Array.from(Array(matrixRowsCount)).map((curr)=> {
+      return Array.from(Array(matrixColsCount * measuresCount))
+    })
+
+    headersDataParts.forEach((currPart) => {
+      if (Array.isArray(currPart)) {
+        const rowPart = transformer.getRowPosition(hierarchy, headersData.rows, currPart)
+        const colPart = transformer.getColPosition(hierarchy, headersData.cols, currPart)
+
+        const measures = currPart.filter((curr,index)=>
+          hierarchy[index].type === 'measures'
+        )
+
+        measures.forEach((currMeasure, index) => {
+          matrix[rowPart][colPart*measures.length + index] = currMeasure
+        })
+      }
+    })
+
+    trs = trs.map((currTr, index) => {
+      const dataTds = matrix[index].map((currRow,index2) => {
+        let tdClassNames = [];
+
+        if (index % 2 === 0) {
+          tdClassNames.push(style.evenRow)
+        }
+
+        if ((index2 + 1) % measuresCount === 0) {
+          tdClassNames.push(style.sectionCol)
+        } else {
+          tdClassNames.push(style.normalCol)
+        }
+
+        tdClassNames.push(style.cell)
+
+        return (<td className={classnames(tdClassNames)} key={index+'.'+index2}>{currRow ? parseFloat(currRow).toFixed(2) : currRow }</td>)
+      })
+      const temp = currTr.concat(dataTds)
+      return temp
+    })
 
     return trs
   }
